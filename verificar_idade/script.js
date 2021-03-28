0d7f92833d57814205969a851a4948c054ed3483
Talyson André <talysonoficial12@gmail.com>
2021-03-28 20:04:14 -0300

    update

---
+++ verificar_idade/script.js
@@ -0,0 +1,34 @@
+window.document.querySelector('input#botao').addEventListener('click', verificar)
+
+function verificar() {
+    var ano = new Date().getFullYear()
+    var ano_formulario = window.document.querySelector('input#nascimento').value
+    var resultado = window.document.querySelector('p#resultado')
+    var imagem = window.document.querySelector('img#imagem')
+
+    if (ano_formulario == 0 || ano_formulario > ano) {
+        window.alert('Digite um ano válido')
+    } else {
+        var sexo = window.document.getElementsByName('sexo')
+        var idade = ano - ano_formulario
+        var gênero = sexo[0].checked ? 'homem' : 'mulher'
+        
+        if (idade >= 0 && idade < 10) {
+            // criança
+            imagem.src = `${gênero == 'homem' ? '_imagens/foto-bebe-m.png' : '_imagens/foto-bebe-f.png'}`
+            resultado.innerHTML = `Detectamos uma criança com ${idade} ${idade == 1 ? 'ano' : 'anos'}`
+        } else if (idade < 21) {
+            // jovem
+            imagem.src = `${gênero == 'homem' ? '_imagens/foto-jovem-m.png' : '_imagens/foto-jovem-f.png'}`
+            resultado.innerHTML = `Detectamos ${gênero == 'homem' ? 'um' : 'uma'} jovem com ${idade} anos`
+        } else if (idade < 50) {
+            // adulto
+            imagem.src = `${gênero == 'homem' ? '_imagens/foto-adulto-m.png' : '_imagens/foto-adulto-f.png'}`
+            resultado.innerHTML = `Detectamos ${gênero == 'homem' ? 'um adulto' : 'uma adulta'} com ${idade} anos`
+        } else {
+            // idoso
+            imagem.src = `${gênero == 'homem' ? '_imagens/foto-idoso-m.png' : '_imagens/foto-idoso-f.png'}`
+            resultado.innerHTML = `Detectamos ${gênero == 'homem' ? 'um idoso' : 'uma idosa'} com ${idade} anos`
+        }
+    }
+}
